bb9e9d0ddc75bb32c46030d9dbe18429162afe54
folomeev1980 <Lazarev0205!980>
2019-12-04 20:33:38 +0300

    servicebot v1.2

--- bot.py
+++ bot.py
@@ -7,16 +7,20 @@ import time
 
 def main():
     t = Telega("981624897:AAFD1wu7bBI4XCR3JHvf9iYqRLPZujpDLJM")
-    usdrur = 63.50
+
+    # Уровни отслеживания
+    usdrur = 64.96
 
     curency_dic = currency_import()
 
     if curency_dic["usdrur"] > usdrur:
+        t.send_message(t.chat_id(), "Превышен уровень пары USD//RUR")
         t.send_message(t.chat_id(), curency_message(currency_import()))
+        usdrur = 0
 
 
 if __name__ == "__main__":
-    schedule.every(10).minutes.do(main)
+    schedule.every(25).minutes.do(main)
 
     while True:
         schedule.run_pending()
