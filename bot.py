d1995bc69fb6d6bcfd36032f4bc6e632667bf3e1
folomeev1980 <Lazarev0205!980>
2019-12-07 00:23:49 +0300

    servicebot v1.2

--- bot.py
+++ bot.py
@@ -9,14 +9,22 @@ def main():
     t = Telega("981624897:AAFD1wu7bBI4XCR3JHvf9iYqRLPZujpDLJM")
 
     # Уровни отслеживания
-    usdrur = 64.96
+    usdrur = 63.50
+    eurusd=1.1100
 
     curency_dic = currency_import()
 
-    if curency_dic["usdrur"] > usdrur:
-        t.send_message(t.chat_id(), "Превышен уровень пары USD//RUR")
+
+    if curency_dic["usdrur"] < usdrur:
+        t.send_message(t.chat_id(), "Пара USD/RUR опустилась ниже 63.5" )
+        t.send_message(t.chat_id(), curency_message(currency_import()))
+
+    if curency_dic["eurusd"] > eurusd:
+        t.send_message(t.chat_id(), "Пара EUR/USD поднялась выше 1.1100" )
         t.send_message(t.chat_id(), curency_message(currency_import()))
         usdrur = 0
+
+
 
 
 if __name__ == "__main__":
